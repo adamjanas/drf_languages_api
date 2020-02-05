da873897bb201312a495a0cbd4b701b147f9716a
adamjanas <a.janas@protonmail.com>
2020-02-05 09:31:56 +0100

    initial commit

---
+++ api_example/restapi_project/restapi_app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Language, Paradigm, Programmer
+from .serializers import LanguageSerializer, ProgrammerSerializer, ParadigmSerializer
+
+class LanguageView(viewsets.ModelViewSet):
+	queryset = Language.objects.all()
+	serializer_class = LanguageSerializer
+	permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+class ProgrammerView(viewsets.ModelViewSet):
+	queryset = Programmer.objects.all()
+	serializer_class = ProgrammerSerializer
+
+class ParadigmView(viewsets.ModelViewSet):
+	queryset = Paradigm.objects.all()
+	serializer_class = ParadigmSerializer
